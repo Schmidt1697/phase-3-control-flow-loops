649ceba9be90f6e8a6f60d7a602fd1f547fcc77f
Anna Schmidt <marshall1697@gmail.com>
2022-10-09 18:17:13 -0600

    commit tree 6b883c941169b69241804b5e87d73b328c94d9c8

--- looping.rb
+++ looping.rb
@@ -1,8 +1,14 @@
+
 def happy_new_year
-  # your code here
+  i = 10;
+  while i >= 1
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
+# # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -16,9 +22,16 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  new_str = ""
+  length = str.length
+  length.times do |i|
+    new_str = str[i] + new_str
+  end
+  new_str
 end
